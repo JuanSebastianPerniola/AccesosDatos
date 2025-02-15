46770e2f53a8e7b16f71a11a7c8a7b1ec999007d
juantravelgate <juan.perniola@travelgate.com>
2025-02-15 15:41:46 +0100

    mi primer commit

---
+++ demo/src/main/java/com/example/demo/Producto.java
@@ -0,0 +1,75 @@
+package com.example.demo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+// Entidad de mi Producto
+@Entity
+//Tabla
+@Table(name = "producto")
+public class Producto {
+    // ID 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    // Nombre de tabla 1 => nombre
+    @Column(nullable = false)
+    private String nombre;
+
+    // Nombre de tabla 2 => descp
+    private String descripcion;
+
+    // Nombre de tabla 3 => precio
+    @Column(nullable = false)
+    private Integer precio;
+
+    // Nombre de tabla 4 => cantidad
+    @Column(nullable = false)
+    private double cantidad;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+}
