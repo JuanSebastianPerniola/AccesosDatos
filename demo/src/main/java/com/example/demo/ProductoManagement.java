057a9b8093a5daaf4692c45efc030788d5942aec
JuanSebastianPerniola <jperniola@nigul.cide.es>
2025-02-18 18:14:14 +0100

    Add port handler

--- demo/src/main/java/com/example/demo/ProductoManagement.java
+++ demo/src/main/java/com/example/demo/ProductoManagement.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 // Interfaz que extiende de JpaRepository para poder realizar operaciones CRUD
 // CRUD => Create, Read, Update, Delete
 // Interfaces son un conjunto de metodos que una clase debe implementar
+// A
 @Repository
 public interface ProductoManagement extends JpaRepository<Producto, Integer> {
 
